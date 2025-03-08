81294e9ca50f1dcab490c9321f9846fbd79a4824
jmcph4 <jack@jmcph4.dev>
2025-03-08 13:41:47 +1000

    Add ExEx scaffold

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,12 @@ use serde::Deserialize;
 use serde_cbor::from_slice;
 use serde_with::{serde_as, skip_serializing_none};
 
+use futures::{Future, TryStreamExt};
+use reth_exex::{ExExContext, ExExEvent, ExExNotification};
+use reth_node_api::FullNodeComponents;
+use reth_node_ethereum::EthereumNode;
+use reth_tracing::tracing::info;
+
 /// Number of bytes that denote the length of the CBOR data
 const CBOR_METADATA_LENGTH_LEN: usize = 2;
 
@@ -117,6 +123,58 @@ fn grab_digest(data: &[u8]) -> Option<Digest> {
     }
 }
 
+/// The initialization logic of the ExEx is just an async function.
+///
+/// During initialization you can wait for resources you need to be up for the ExEx to function,
+/// like a database connection.
+async fn exex_init<Node: FullNodeComponents>(
+    ctx: ExExContext<Node>,
+) -> eyre::Result<impl Future<Output = eyre::Result<()>>> {
+    Ok(exex(ctx))
+}
+
+/// An ExEx is just a future, which means you can implement all of it in an async function!
+///
+/// This ExEx just prints out whenever either a new chain of blocks being added, or a chain of
+/// blocks being re-orged. After processing the chain, emits an [ExExEvent::FinishedHeight] event.
+async fn exex<Node: FullNodeComponents>(
+    mut ctx: ExExContext<Node>,
+) -> eyre::Result<()> {
+    while let Some(notification) = ctx.notifications.try_next().await? {
+        match &notification {
+            ExExNotification::ChainCommitted { new } => {
+                info!(committed_chain = ?new.range(), "Received commit");
+            }
+            ExExNotification::ChainReorged { old, new } => {
+                info!(from_chain = ?old.range(), to_chain = ?new.range(), "Received reorg");
+            }
+            ExExNotification::ChainReverted { old } => {
+                info!(reverted_chain = ?old.range(), "Received revert");
+            }
+        };
+
+        if let Some(committed_chain) = notification.committed_chain() {
+            ctx.events.send(ExExEvent::FinishedHeight(
+                committed_chain.tip().num_hash(),
+            ))?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn start_exex() -> eyre::Result<()> {
+    reth::cli::Cli::parse_args().run(|builder, _| async move {
+        let handle = builder
+            .node(EthereumNode::default())
+            .install_exex("Minimal", exex_init)
+            .launch()
+            .await?;
+
+        handle.wait_for_node_exit().await
+    })
+}
+
 #[derive(Clone, Debug, Parser)]
 struct Opts {
     #[clap(short, long, action)]
@@ -129,11 +187,12 @@ struct Opts {
     pub bytecode: Option<PathBuf>,
 }
 
-fn main() -> eyre::Result<()> {
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
     let opts = Opts::parse();
 
     if opts.live {
-        todo!()
+        start_exex().await?;
     } else {
         let bytes = if let Some(path) = opts.bytecode {
             if opts.raw {
