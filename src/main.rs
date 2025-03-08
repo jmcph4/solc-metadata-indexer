90751972a4b3ea58a14f4475523cf9d7cbfc632e
jmcph4 <jack@jmcph4.dev>
2025-03-08 12:45:26 +1000

    Code quality

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,17 @@ use serde::Deserialize;
 use serde_cbor::from_slice;
 use serde_with::{serde_as, skip_serializing_none};
 
-const IPFS_DIGEST_LEN: usize = 43;
-const SWARM_DIGEST_LEN: usize = 43; /* TODO(jmcph4): placeholder */
+/// Number of bytes that denote the length of the CBOR data
 const CBOR_METADATA_LENGTH_LEN: usize = 2;
 
 #[serde_as]
 #[skip_serializing_none]
 #[derive(Clone, Debug, Deserialize)]
+/* We must tolerate the explicit lifetime here due to `serde_cbor`'s idea of
+ * which Rust types map to which CBOR types. Specifically, `serde_cbor`
+ * considers a `Vec<u8>` to be a byte *array* not a *sequence*. We need to
+ * deserialise the latter, hence the `&[u8]`.
+ * */
 struct CanonicalMetadata<'a> {
     pub ipfs: Option<&'a [u8]>,
     _solc: Option<&'a [u8]>,
@@ -31,6 +35,10 @@ impl fmt::Display for Digest {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Digest::Ipfs(bytes) => {
+                /* Critically, the human-readable CID (i.e., that one might use
+                 * to access via a gateway, for instance), is the
+                 * Base58-encoded form of the IPFS digest byte sequence
+                 * */
                 write!(f, "ipfs://{}", bs58::encode(&bytes).into_string())
             }
             Digest::Swarm(bytes) => write!(f, "bzz://{}", hex::encode(bytes)),
@@ -62,7 +70,7 @@ fn grab_cbor_data(data: &[u8]) -> Option<&[u8]> {
     cbor_metadata_length(data).and_then(|n| {
         if data.len() >= n {
             Some(
-                &data[(data.len() - (n + 2))
+                &data[(data.len() - (n + CBOR_METADATA_LENGTH_LEN))
                     ..(data.len() - CBOR_METADATA_LENGTH_LEN)],
             )
         } else {
@@ -82,14 +90,12 @@ fn grab_ipfs_digest(data: &[u8]) -> Option<Vec<u8>> {
         .and_then(|metadata| metadata.ipfs.map(|ipfs| ipfs.to_vec()))
 }
 
+#[inline]
 fn grab_swarm_digest(data: &[u8]) -> Option<Vec<u8>> {
-    if data.len() >= SWARM_DIGEST_LEN {
-        Some(data[data.len() - IPFS_DIGEST_LEN..].to_vec())
-    } else {
-        None
-    }
+    todo!()
 }
 
+#[inline]
 fn grab_digest(data: &[u8]) -> Option<Digest> {
     match (grab_ipfs_digest(data), grab_swarm_digest(data)) {
         (Some(ipfs), None) => Some(Digest::Ipfs(ipfs)),
